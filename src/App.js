c3fa14cc04c2921e42cdc9a09d46a0f9d604151b
maflooty <mmaflooty@gmail.com>
2020-03-06 23:15:40 +0100

    Arrange-Game

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GameBody from "./Components/GameBody";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <GameBody />
+      </header>
+    </div>
+  );
+}
+
+export default App;
